b373011698011c7c4592bfee36649658ad634ba6
Aleksey Belchenkov <u608110@gmail.com>
2020-04-01 14:04:06 +0300

    Add Labels, Entries

---
+++ part_manager.py
@@ -0,0 +1,42 @@
+from tkinter import *
+
+# Create window object
+app = Tk()
+app.title('Part Manager')
+app.geometry('700x350')
+
+# Part
+part_text = StringVar()
+part_label = Label(app, text='Part Name', font=('bold', 14), pady=20)
+part_label.grid(row=0, column=0, sticky=W)
+part_entry = Entry(app, textvariable=part_text)
+part_entry.grid(row=0, column=1)
+
+# Customer
+customer_text = StringVar()
+customer_label = Label(app, text='Customer Name', font=('bold', 14), pady=20)
+customer_label.grid(row=0, column=2, sticky=W)
+customer_entry = Entry(app, textvariable=part_text)
+customer_entry.grid(row=0, column=3)
+
+# Retailer
+retailer_text = StringVar()
+retailer_label = Label(app, text='Retailer Name', font=('bold', 14), pady=20)
+retailer_label.grid(row=1, column=0, sticky=W)
+retailer_entry = Entry(app, textvariable=part_text)
+retailer_entry.grid(row=1, column=1)
+
+# Price
+price_text = StringVar()
+price_label = Label(app, text='Price', font=('bold', 14), pady=20)
+price_label.grid(row=1, column=2, sticky=W)
+price_entry = Entry(app, textvariable=part_text)
+price_entry.grid(row=1, column=3)
+
+
+
+
+
+
+# Start program
+app.mainloop()
